f162ed15539a9f85410a5bd07266b3e035f70a61
Abdulrehman <you@example.com>
2024-10-09 22:10:22 +0500

    Rout App

---
+++ app/About/page.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+      <div>
+        <h1>This is a Router About page from <strong> AbdulRehman Shaikh</strong></h1>
+      </div>
+    );
+  }
+  
---
+++ app/Header/page.tsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+      <div>
+        <h1>This is a Router Header page from <strong> AbdulRehman Shaikh</strong></h1>
+      </div>
+    );
+  }
+  
