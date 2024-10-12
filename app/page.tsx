595db60a131a49c823adb9e6ec99e7344162aa2b
Abdulrehman shaikh <160644627+Abdulrehman-Shaikh1010@users.noreply.github.com>
2024-10-12 11:22:08 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-'use client'
 import React from "react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
 export default function Home(){
-  const route = useRouter();
   return(
     <div>
       <h1>This is a Router Home page from <strong> AbdulRehman Shaikh</strong></h1>
